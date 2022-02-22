590e989cbe49d586c418f26649b02ed5892e3da9
oquendito <86526751+oquendito@users.noreply.github.com>
2022-02-22 01:09:17 -0300

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,24 @@
 import React from 'react'
-import styled from 'styled-components'
+import './StyledNav.css'
 
-function Navbar() {
+const Navbar = () => {
+
   return (
       <>
-    <NavMenu>
-    <h2>FeliFelinos</h2>
-    <div>
-        <a href="/">Inicio</a>
-        <a href="/">Catalogo</a>
-        <a href="/">About</a>
-        <a href="/">Contacto</a>
-    </div>
-    </NavMenu>
+    <header>
+      <nav>
+        <h1 id="main-logo" >
+          <span>FeliFelinos</span>
+        </h1>
+        <ul id="main-menu">
+          <li>Inicio</li>
+          <li>Catálogo</li>
+          <li>Contacto</li>
+        </ul>
+      </nav>
+    </header>
     </>
   )
 }
 
 export default Navbar
-
-const NavMenu = styled.nav`
-h2{
-    color; black;
-    font-weight: 400;
-}
-padding: .4rem;
-background-color: #333;
-display: flex;
-align-items: center;
-justify-content: space-between;
-a{
-    color: white;
-    text-decoration: none;
-    margin-right: 1rem;
-}
-`
